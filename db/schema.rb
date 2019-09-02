24f645a8081c5527ae09049a3c958e21b07630a2
S-Toky <stokyr29@gmail.com>
2019-09-02 17:38:05 +0300

    add signIn and signUp

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_02_131744) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -38,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_09_02_131744) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
