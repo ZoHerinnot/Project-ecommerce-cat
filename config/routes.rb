3226ca83cab5dd0a9da26c7fa2a5d366332e7fce
RFaly <ramahafaly2.0@gmail.com>
2019-09-03 14:38:12 +0300

    Back du show panier

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
   resources :items, only: [:show]
 
-  
+  resources :carts
+
+
+
+
+
+
 end
