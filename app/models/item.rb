9371eb1a1d22b88c4a0f53bbf8303281c4fb1baf
RFaly <ramahafaly2.0@gmail.com>
2019-09-06 12:08:28 +0300

    edit seed

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,10 @@ class Item < ApplicationRecord
     has_many :list_commands
     has_many :carts, through: :list_commands
     has_one_attached :picture
-    belongs_to :user
+
     has_many :join_table_order_items
 		has_many :orders, through: :join_table_order_items
-    
+
     validates :title,  presence: true,length: {maximum: 40}
     validates :description, presence: true, length: {maximum: 500}
     validates :image_url, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_one :cart
   has_many :orders
   has_one_attached :avatar
-  has_many :items
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
