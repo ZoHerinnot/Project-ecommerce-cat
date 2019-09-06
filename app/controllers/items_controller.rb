9a72a4f2c76f83d7c37c3b6eb4c6f9e5a8af33cd
tokynomenaja <nomenjatoky21@gmail.com>
2019-09-06 11:32:41 +0300

    update back user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,17 @@
 class ItemsController < ApplicationController
-	
   def show
   	@item = Item.find(params[:id])
+  end
+
+  def destroy
+  	@item = Item.find(params[:id])
+  	@item.destroy
+
+  	respond_to do |format|
+  		format.js {}
+  		format.html{
+  			redirect_to user_path		
+  		}
+  	end
   end
 end
